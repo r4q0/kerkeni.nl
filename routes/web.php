53ae70ccea728b8527225362a04b8f7f9f984512
r5q0 <bilalemailsss@gmail.com>
2024-07-15 14:05:48 +0200

    Update welcome.blade.php to include dynamic age variable in the view

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectsController;
+use Illuminate\Support\Carbon;
 
 Route::get('/', function () {
-    return view('welcome');
+    $age = intval(Carbon::create(2007, 1, 11)->diffInYears(Carbon::now()));
+    return view('welcome', ['age' => $age]);
 })->name('home');
 
 Route::get('/projects', function () {
