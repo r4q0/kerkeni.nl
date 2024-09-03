922f882c259ffe469bcda3991035ea62fdb341be
r5q0 <bilalemailsss@gmail.com>
2024-09-03 18:32:12 +0200

    chore: Update endeavours.blade.php layout and project details

--- resources/views/endeavours.blade.php
+++ resources/views/endeavours.blade.php
@@ -18,7 +18,7 @@
     </a>
     <div class="flex justify-center items-center">
 
-        <div class="grid grid-cols-1 w-[75vw] h-[179vh] gap-20">
+        <div class="grid grid-cols-1 w-[70vw] h-[150vh] gap-20">
             <div class="bg-[#061434] rounded-[70px] blur select-none">
                 <h2 class="text-center font-bold font-mono text-8xl text-white pt-10">Krate</h2>
                 <p class="text-white text-xl font-mono max-w-[40vw] pt-10 text-center mx-auto">Krate is my main focus. It is a cloud compute service catered to high-compute markets such as AI, rendering, and more. I aspire to innovate within the compute industry.</p>
