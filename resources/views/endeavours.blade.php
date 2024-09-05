8cdcb7b6b45fae1aba72b646499d5e8dc9bc394f
r5q0 <bilalemailsss@gmail.com>
2024-09-05 20:45:52 +0200

    Update page titles in blade templates

--- resources/views/endeavours.blade.php
+++ resources/views/endeavours.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Bilal | Endeavors</title>
 </head>
 
 <body class="bg-[#060522]">
